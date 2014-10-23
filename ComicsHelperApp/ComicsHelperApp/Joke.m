11fe64d1899c1279779626825d7cdc8e851e74d1
Terry Bu <ktbucifer@gmail.com>
2014-10-23 18:20:34 -0400

    Started working on joke app

---
+++ ComicsHelperApp/ComicsHelperApp/Joke.m
@@ -0,0 +1,13 @@
+//
+//  Joke.m
+//  ComicsHelperApp
+//
+//  Created by Aditya Narayan on 10/23/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import "Joke.h"
+
+@implementation Joke
+
+@end
---
+++ ComicsHelperApp/ComicsHelperApp/JokeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JokeTableViewController.h
+//  ComicsHelperApp
+//
+//  Created by Aditya Narayan on 10/23/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JokeTableViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end

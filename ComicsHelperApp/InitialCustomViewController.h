bbd8338819197501bccc863af3b03532faecca4d
Terry Bu <ktbucifer@gmail.com>
2014-10-23 12:04:47 -0400

    got some autolayout practice done

---
+++ ComicsHelperApp/InitialCustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitialCustomViewController.h
+//  ComicsHelperApp
+//
+//  Created by Aditya Narayan on 10/22/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialCustomViewController : UIViewController
+
+@end
---
+++ ComicsHelperApp/InitialCustomViewController.m
@@ -0,0 +1,37 @@
+//
+//  InitialCustomViewController.m
+//  ComicsHelperApp
+//
+//  Created by Aditya Narayan on 10/22/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import "InitialCustomViewController.h"
+
+@interface InitialCustomViewController ()
+
+@end
+
+@implementation InitialCustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
